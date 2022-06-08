4d7aa7dabe2407dddb0f4c1346647f0fd499a01f
James Manalang <james.e.manalang@gmail.com>
2022-06-09 02:43:37 +0800

    Setup app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="search" element={<Search />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/Search.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Search = () => (
+  <>
+    <h1>Search Page</h1>
+  </>
+);
+
+export default Search;
